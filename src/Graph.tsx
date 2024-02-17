5c487edfb5cbb0a36318d54cac7d05b4055a60d7
Neal L <rlawton.cs@gmail.com>
2024-02-16 23:19:47 -0800

    Changed the graph to track the ratio between two stocks and added upper and lower bounds to it.

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Table } from '@finos/perspective';
+import { Table, TableData } from '@finos/perspective';
 import { ServerRespond } from './DataStreamer';
 import { DataManipulator } from './DataManipulator';
 import './Graph.css';
@@ -23,10 +23,18 @@ class Graph extends Component<IProps, {}> {
     const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
 
     const schema = {
-      stock: 'string',
-      top_ask_price: 'float',
-      top_bid_price: 'float',
+    // Before, we were tracking the changes in the two stocks' top_ask_price and
+    // top_bid_price over time.
+    // What we're actually interested in is the ratio of the price of the two stocks
+    // over time.  We also want to track the upper_bound and lower_bound, and be alerted
+    // when these bounds are crossed.
+      price_abc: 'float',
+      price_def: 'float',
+      ratio: 'float',
       timestamp: 'date',
+      upper_bound: 'float',
+      lower_bound: 'float',
+      trigger_alert: 'float',
     };
 
     if (window.perspective && window.perspective.worker()) {
@@ -35,24 +43,33 @@ class Graph extends Component<IProps, {}> {
     if (this.table) {
       // Load the `table` in the `<perspective-viewer>` DOM reference.
       elem.load(this.table);
+      // The kind of graph we want to visualize the data with.
       elem.setAttribute('view', 'y_line');
-      elem.setAttribute('column-pivots', '["stock"]');
+      // Allows us to map each datapoint based on its timestamp.  Without it, the
+      // x-axis would be blank.
       elem.setAttribute('row-pivots', '["timestamp"]');
-      elem.setAttribute('columns', '["top_ask_price"]');
+      // Allows us to focus on a particular part of a datapoint's data along
+      // the y-axis.  We want to track lower_bound, upper_bound, and trigger_alert.
+      elem.setAttribute('columns', '["ratio", "lower_bound", "upper_bound", "trigger_alert"]');
+      // Allows us to handle the duplicate data and consolidate them into one data point.
       elem.setAttribute('aggregates', JSON.stringify({
-        stock: 'distinctcount',
-        top_ask_price: 'avg',
-        top_bid_price: 'avg',
+        price_abc: 'avg',
+        price_def: 'avg',
+        ratio: 'avg',
         timestamp: 'distinct count',
+        upper_bound: 'avg',
+        lower_bound: 'avg',
+        trigger_alert: 'avg',
       }));
     }
   }
 
+  // Gets executed whenever the component updates (ie. when the graph gets new data.)
   componentDidUpdate() {
     if (this.table) {
-      this.table.update(
+      this.table.update([
         DataManipulator.generateRow(this.props.data),
-      );
+      ] as unknown as TableData);
     }
   }
 }
